ab749a18a272a8f9d1758c32e2dbdd2e7dd32e9a
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2020-11-24 00:46:22 -0300

    First try

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
-import { View, Image } from 'react-native';
+import { View, Image, Alert } from 'react-native';
 
 import formatValue from '../../utils/formatValue';
 import { useCart } from '../../hooks/cart';
@@ -35,14 +35,23 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO
+      try {
+        const response = await api.get('/products');
+
+        setProducts(response.data);
+      } catch (err) {
+        Alert.alert(
+          'Erro na requisição',
+          'Ocorreu um erro ao carregar os produtos. Tente novamente.',
+        );
+      }
     }
 
     loadProducts();
   }, []);
 
   function handleAddToCart(item: Product): void {
-    // TODO
+    addToCart(item);
   }
 
   return (
